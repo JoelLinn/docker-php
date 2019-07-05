fd450804d90136c0cc7ff1ce3e2c8fea5d9eac82
Joel Linn <jl@conductive.de>
2019-07-05 14:34:08 +0200

    Added debian buster support (no openssl for PHP 5.x)

--- update.sh
+++ update.sh
@@ -197,10 +197,17 @@ for version in "${versions[@]}"; do
 					-e '/log_limit/d' \
 					"$version/$suite/$variant/Dockerfile"
 			fi
-                        if [ "$majorVersion" -lt '7' -a "$suite" = 'stretch' ]; then
-                                # php 5.x does not build with openssl 1.1
-                                sed -ri 's/libssl-dev/libssl1.0-dev/' "$version/$suite/$variant/Dockerfile"
-                        fi
+			if [ "$majorVersion" -lt '7' -a "$suite" = 'stretch' ]; then
+				# php 5.x does not build with openssl 1.1
+				sed -ri 's/libssl-dev/libssl1.0-dev/' "$version/$suite/$variant/Dockerfile"
+			fi
+			if [ "$majorVersion" -lt '7' -a "$suite" = 'buster' ]; then
+				# buster does not ship with openssl 1.0 any more so remove it
+				sed -ri \
+					-e '/libssl-dev/d' \
+					-e '/--with-openssl/d' \
+					"$version/$suite/$variant/Dockerfile"
+			fi
 
 			# remove any _extra_ blank lines created by the deletions above
 			gawk '
